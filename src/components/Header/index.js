1ccd0887c2f3fdc35e00f338bdf36fc7b4f3f772
alecfirstcode2021 <78454908+alecfirstcode2021@users.noreply.github.com>
2021-05-20 20:03:39 -0500

    add Wrapper components

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>View your entire employee by clicking on each headong or searching the information of your employee</p>
+        </div>
+    )
+}
+
+export default Header;
+
+
